f615e7f902ec48acd39f6111803f7e503ee70e5c
SilverioPacheco <jefecitochung18@gmail.com>
2024-03-15 08:24:04 -0400

    tarea

--- src/graph.js
+++ src/graph.js
@@ -1,6 +1,6 @@
-
 import Vertex from "./graphs/Vertex";
-import bfs  from './graphs/paths/Bfs'
+import bfs  from './graphs/paths/Bfs';
+import dfs from './graphs/paths/Dfs';
 function initGraph() {
     const v1 = new Vertex('v1');
     const v2 = new Vertex('v2');
@@ -21,16 +21,6 @@ function initGraph() {
     v6.addNeighbor(v3);
     v6.addNeighbor(v4);
     v6.addNeighbor(v7);
-    // v1.addNeighbor(v2);
-    // v1.addNeighbor(v3);
-    // v1.addNeighbor(v4);
-    // v2.addNeighbor(v3);
-    // v3.addNeighbor(v4);
-    // v3.addNeighbor(v2);
-    // v3.addNeighbor(v5);
-    // v5.addNeighbor(v3);
-    // v5.addNeighbor(v2);
-    // run dfs
     bfs(v1);
 }
 initGraph();
--- src/graphs/paths/Dfs.js
+++ src/graphs/paths/Dfs.js
@@ -1,7 +1,23 @@
 
 // Homework, this presentation will be on fridays
 // in lab.
-const dfs = () => {
-    //super codigo
+const dfs = (source) => {
+    source.setVisited(true);
+    const vertexLine = [];
+    vertexLine.push(source);
+    while (!isEmpty(vertexLine)) {
+        const firstVertex = vertexLine.shift();
+        firstVertex.print();
+        firstVertex.getVertexList().forEach(vertex => {
+            if (!vertex.getVisited()) {
+                vertex.setVisited(true);
+                vertexLine.push(vertex);
+            }
+            
+        });
+    }
+}
+const isEmpty = (vertexLine) => {
+    return vertexLine.length === 0;
 }
 export default dfs;
